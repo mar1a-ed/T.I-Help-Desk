3d44c2eef7fa6e896356c42881fe8d267d4c8d08
Maria Eduarda <medu.correa29@gmail.com>
2026-09-20 22:11:49 -0300

    security: implementado SecurityFilterChain

--- src/main/java/com/maria/help_desk/config/SecurityConfig.java
+++ src/main/java/com/maria/help_desk/config/SecurityConfig.java
@@ -2,10 +2,28 @@ package com.maria.help_desk.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(authorization -> authorization
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
